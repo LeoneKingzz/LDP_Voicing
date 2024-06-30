973efa7a6de8103fe134d886a106ab68049be132
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-06-30 13:49:34 +0100

    Update hook.cpp

--- src/hook.cpp
+++ src/hook.cpp
@@ -40,3 +40,20 @@ namespace hooks
 
 }
 
+// const auto caster = a_defender->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant);
+// float a_reprisal = (EldenParry::GetSingleton()->AttackerBeatsParry(a_aggressor, a_defender));
+// auto bHasEldenParryPerk2 = a_defender->HasPerk(RE::BGSPerk::LookupByEditorID("ORD_Bck20_TimedBlock_Perk_50_OrdASISExclude")->As<RE::BGSPerk>());
+// auto bHasEldenParryPerk1 = a_defender->HasPerk(RE::BGSPerk::LookupByEditorID("ORD_Bck20_TimedBlock_Perk_20_OrdASISExclude")->As<RE::BGSPerk>());
+// if (bHasEldenParryPerk2 || bHasEldenParryPerk1)
+// {
+//     RE::MagicItem *eldenArmorSpell = nullptr;
+//     if (bHasEldenParryPerk2 == true)
+//     {
+//         eldenArmorSpell = RE::TESForm::LookupByEditorID<RE::MagicItem>("ORD_Bck_TimedBlock_Spell_Proc_2");
+//     }
+//     else if (bHasEldenParryPerk1)
+//     {
+//         eldenArmorSpell = RE::TESForm::LookupByEditorID<RE::MagicItem>("ORD_Bck_TimedBlock_Spell_Proc");
+//     }
+//     caster->CastSpellImmediate(eldenArmorSpell, true, a_defender, 1, false, 45, a_defender);
+// }
